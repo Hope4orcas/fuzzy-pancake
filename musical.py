6742240ba9dc351519b046ff4d17e6b5af3f811d
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-02-21 17:54:33 +0100

    Update musical.py

--- musical.py
+++ musical.py
@@ -9,7 +9,7 @@ choice_alt=["https://www.youtube.com/watch?v=dn5rSbHGsHY",
             "https://www.youtube.com/watch?v=qolmz4FlnZ0",
             "https://www.youtube.com/watch?v=iggmiF7DNoM"
             ]
- choice_pop=["https://www.youtube.com/watch?v=wV1FrqwZyKw",
+choice_pop=["https://www.youtube.com/watch?v=wV1FrqwZyKw",
             "https://www.youtube.com/watch?v=QYh6mYIJG2Y",
             "https://www.youtube.com/watch?v=e-ORhEE9VVg"
             ]
