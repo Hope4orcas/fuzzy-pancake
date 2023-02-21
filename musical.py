20eca26a19fb62b7416870f640c83d4e8fe72d25
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-02-21 17:51:13 +0100

    Create musical.py

---
+++ musical.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import random
+st.title("Elija su tipo de música")
+
+respuesta=st.radio("¿Género de música favorito?", opciones)
+opciones=("Alt","Pop","Rock", "Reggaeton","Vocaloid","Kpop")
+
+choice_alt=["https://www.youtube.com/watch?v=dn5rSbHGsHY",
+            "https://www.youtube.com/watch?v=qolmz4FlnZ0",
+            "https://www.youtube.com/watch?v=iggmiF7DNoM"
+            ]
+ choice_pop=["https://www.youtube.com/watch?v=wV1FrqwZyKw",
+            "https://www.youtube.com/watch?v=QYh6mYIJG2Y",
+            "https://www.youtube.com/watch?v=e-ORhEE9VVg"
+            ]
+
+if respuesta=="Alt":
+  st.write("maravilloso")
+  st.video(random.choice(choice_alt))
+if respuesta=="Pop":
+  st.write("gg")
+  st.video(random.choice(choice_pop))
