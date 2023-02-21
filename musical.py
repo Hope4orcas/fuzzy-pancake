7af9c9263abcdddaf91bc3ce8d7c9ea5116633e4
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-02-21 18:04:22 +0100

    Update musical.py

--- musical.py
+++ musical.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 st.title("Elija su tipo de música")
 
-opciones=("Alt","Pop","Rock", "Reggaeton","Vocaloid","Kpop")
+opciones=("Alt","Pop", "Reggaeton","Vocaloid","Kpop")
 
 respuesta=st.radio("¿Género de música favorito?", opciones)
 
@@ -14,10 +14,23 @@ choice_pop=["https://www.youtube.com/watch?v=wV1FrqwZyKw",
             "https://www.youtube.com/watch?v=QYh6mYIJG2Y",
             "https://www.youtube.com/watch?v=e-ORhEE9VVg"
             ]
-
+choice_kpop=["https://www.youtube.com/watch?v=pyf8cbqyfPs",
+            "https://www.youtube.com/watch?v=KhTeiaCezwM",
+            "https://www.youtube.com/watch?v=Jh4QFaPmdss"
+            ]
+choice_voc=["https://www.youtube.com/watch?v=ScSW9C3DF18",
+            "https://www.youtube.com/watch?v=Av04XfLtAcU",
+            "https://www.youtube.com/watch?v=TNf3GPizM58"
+            ]
 if respuesta=="Alt":
   st.write("maravilloso")
   st.video(random.choice(choice_alt))
 if respuesta=="Pop":
   st.write("gg")
   st.video(random.choice(choice_pop))
+if respuesta=="Kpop":
+  st.write("OMG")
+  st.video(random.choice(choice_kpop))
+if respuesta=="Vocaloid":
+  st.write("Yasssss")
+  st.video(random.choice(choice_voc))
